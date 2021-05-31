04e0bce6f10d3011744d4e8cab4fbc85ae4b31db
nithin-sudo <nithinkrishnasathram@gmail.com>
2021-05-31 08:33:50 +0530

    adding

---
+++ src/javapractice/HelloJava.java
@@ -0,0 +1,11 @@
+package javapractice;
+
+public class HelloJava {
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+	}
+	public int add(int a,int b) {
+		return a+b;
+	}
+
+}
